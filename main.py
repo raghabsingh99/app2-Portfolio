f930a148c9e42c114f0dcb2ba68610518cc85460
raghab singh <raghabsingh99@gmail.com>
2024-07-17 20:56:27 -0500

    Add description on the new columns

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ with col1:
 
 with col2:
     st.title("Raghab Singh")
-    content = """Hi I am raghab singh. I am leraning python programming. In the context of data, "DE" typically stands for Data Engineer. A Data Engineer is responsible for designing, building, and maintaining the infrastructure and systems that enable the collection, storage, and analysis of data. 
+    content = """Hi I am raghab singh. I am leraning python programming. In the context of data, "DE" typically stands 
+    for Data Engineer. A Data Engineer is responsible for designing, building, and maintaining the infrastructure and systems 
+    that enable the collection, storage, and analysis of data. 
     Their primary focus is on ensuring that data is accessible, reliable, and ready for use by data scientists, analysts, 
     and other stakeholders."""
+
     st.info(content)
+
+content2 = """Below you can find some of the app I have built in python. Feel Free to contact me!Their primary focus is on ensuring that data is accessible, reliable, and ready for use by data scientists, analysts, 
+    and other stakeholders.
+"""
+st.write(content2)
